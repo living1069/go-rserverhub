2ec4062d1f2d26bfc1b9141e83c3991792adcd6b
vastik <svoutjekk@gmail.com>
2019-08-03 16:45:45 +0300

    UPD: replaced tabs by spaces

--- app/db.go
+++ app/db.go
@@ -1,14 +1,24 @@
 package app
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 func CreateConnection() {
-	var uri = "host=10.8.0.1 port=5432 user=dev password=dev dbname=rserverhub sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	password := os.Getenv("DB_PASSWORD")
+	user := os.Getenv("DB_USER")
+	db := os.Getenv("DB_NAME")
+
+	var uri = fmt.Sprintf("host=%s port=%s user=%d password=%s dbname=%s sslmode=disable",
+		host, port, user, password, db)
+
 	var err error
 	DB, err = gorm.Open("postgres", uri)
 
